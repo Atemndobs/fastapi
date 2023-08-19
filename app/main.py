1a51c048492fbc76920379ccec9241ac4d33d160
Atemndobs <atemndobs@yahoo.com>
2023-08-20 00:22:39 +0200

    update fastapi to v1.3

--- app/main.py
+++ app/main.py
@@ -10,6 +10,16 @@ app = FastAPI()
 # Load the CSV data into a DataFrame
 df = pd.read_csv("/code/app/quiz_data.csv")
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
 @app.get("/questions/{question_id}")
 def get_question(question_id: int):
     """Fetch a specific question and its choices."""
