fa5d961e415dd7c601c8e282cadc4d132b0d1e09
Atemndobs <atemndobs@yahoo.com>
2023-08-20 00:38:56 +0200

    update fastapi to v1.4

--- app/main.py
+++ app/main.py
@@ -28,10 +28,32 @@ def get_question(question_id: int):
     if start_idx >= len(df):
         raise HTTPException(status_code=404, detail="Question not found")
     question_data = df.iloc[start_idx:end_idx]
+
+    # Create a link for the next question
+    next_question_id = question_id + 1
+    # Check if there's a next question
+    next_start_idx = (next_question_id - 1) * 4
+    if next_start_idx >= len(df):
+        # No more questions
+        next_question_link = None
+    else:
+        next_question_link = f"/questions/{next_question_id}"
+
+    # Create a link for the previous question
+    prev_question_id = question_id - 1
+    if prev_question_id <= 0:
+        # No previous questions
+        prev_question_link = None
+    else:
+        prev_question_link = f"/questions/{prev_question_id}"
+
     return {
         "question": question_data["Question"].iloc[0],
-        "choices": question_data["Choices"].tolist()
+        "choices": question_data["Choices"].tolist(),
+        "next_question_link": next_question_link,
+        "prev_question_link": prev_question_link  # Add this line
     }
+
 
 class Answer(BaseModel):
     answer: int
